d5925a42cecb317aab26cbca5e1b1e7010a7fca0
Fabio <yfabioyasuo@gmail.com>
2025-10-30 19:48:31 -0300

    initiated the improvement of the mobile homePage

--- src/member/JobItem.jsx
+++ src/member/JobItem.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router";
 
+import { FaBriefcase, FaLocationDot } from "react-icons/fa6";
+
 const JobItem = ({ job }) => {
   const [jobDetail, setJobDetail] = useState(false);
 
@@ -19,6 +21,9 @@ const JobItem = ({ job }) => {
         <span className="font-semibold">Show Job Detail</span>
       </button>
 
+      {/* 
+          This piece of jsx is for mobile and small devices 
+      */}
       {jobDetail && (
         <div className="max-h-96 border border-gray-300 text-xl space-y-2 rounded mt-2 p-4 overflow-y-scroll transition-all duration-700 ease-in-out md:hidden">
           <h2 className="text-4xl font-bold capitalize">{job.title}</h2>
@@ -30,14 +35,10 @@ const JobItem = ({ job }) => {
           </p>
           <p className="text-gray-500">{job.address}</p>
           <p className="font-semibold">{job.jobType}</p>
-          <p className="font-thin text-gray-500">
-            You must create an JobHunt account before continuing to the company
-            website to apply
-          </p>
           <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Job Detail</h2>
             <div className="flex items-center gap-8">
-              <i className="fa-solid fa-briefcase"></i>
+              <FaBriefcase size={16} />
               <div>
                 <h3 className="inline">Job Type</h3>
                 <p className="bg-gray-200 text-sm text-center text-slate-800 py-1 px-2 rounded">
@@ -49,13 +50,21 @@ const JobItem = ({ job }) => {
           <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Location</h2>
             <div className="flex items-center gap-8">
-              <i className="fa-solid fa-location-dot"></i>
+              <FaLocationDot size={16} />
               <p className="text-gray-500">{job.location}</p>
             </div>
           </div>
           <div className="flex flex-col gap-2">
             <h2 className="text-2xl font-bold">Full job description</h2>
             <p className="text-gray-500 text-justify">{job.description}</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-bold">Full job responsibilities</h2>
+            <p className="text-gray-500 text-justify">{job.responsibilities}</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-bold">Full job requirements </h2>
+            <p className="text-gray-500 text-justify">{job.requirements}</p>
           </div>
         </div>
       )}
