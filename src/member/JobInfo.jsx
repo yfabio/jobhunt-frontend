fc4cd504cd26c513cd1e6fba8eede0cfbebaa3d5
Fabio <yfabioyasuo@gmail.com>
2025-10-30 14:25:47 -0300

    improved the jobInfo by adding an apply button with a modal confirmation, and updated the jobs.js file with new data

--- src/member/JobInfo.jsx
+++ src/member/JobInfo.jsx
@@ -1,35 +1,74 @@
+import { useState } from "react";
 import { Navigate, useParams } from "react-router";
+import { FaBriefcase, FaLocationDot } from "react-icons/fa6";
+
 import { useJobsCtx } from "../context/JobsContext";
+import { useAuthCtx } from "../context/AuthContext";
+
+import Modal from "../components/Modal";
+import ButtonsAction from "../components/ButtonsAction";
 
 const JobInfo = () => {
+  const [modal, setModal] = useState({ open: false, job: {} });
+  const { user } = useAuthCtx();
   const { getJobById } = useJobsCtx();
 
   const params = useParams();
 
   const job = getJobById(Number(params.id));
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("user who applied ", user.role);
+    setModal({ open: false, job: {} });
+  };
+
   return (
     <>
+      {modal.open && (
+        <Modal
+          title={"Job confirmation"}
+          close={() => setModal({ open: false, job: {} })}>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-2">
+              <p className="font-bold text-center text-rose-500">
+                Are you sure you want apply for this job?
+              </p>
+              <div className="border-[1px] border-gray-100 p-6 rounded">
+                <h2 className="font-bold text-lg">{modal.job.title}</h2>
+                <span className="text-gray-500">{modal.job.company}</span>
+                <p className="text-gray-500">{modal.job.location}</p>
+                <p className="font-semibold">{modal.job.jobType}</p>
+                <p className="text-gray-500">{modal.job.address}</p>
+              </div>
+              <ButtonsAction
+                onCancel={() => setModal({ open: false, job: {} })}
+                btnLeft="No"
+                btnRight="Yes"
+              />
+            </div>
+          </form>
+        </Modal>
+      )}
       {job ? (
         <section className="hidden flex-2 md:block">
-          <div className="flex flex-col border border-gray-300 text-xl rounded p-4 gap-3 flex-2">
+          <div className="flex flex-col border border-gray-300 rounded p-4 gap-2 flex-2">
             <h2 className="text-4xl font-bold capitalize">{job.title}</h2>
-            <p className="space-x-1">
-              <span className="text-gray-500">{job.company}</span>
-              <i className="fa-solid fa-circle text-[4px] align-middle p-2"></i>
-              <span className="text-2xl font-semibold">{job.rating}</span>
-              <i className="fa-solid fa-star text-xl"></i>
-            </p>
+            {user.role === "member" && (
+              <button
+                onClick={() => setModal({ open: true, job })}
+                type="button"
+                className="w-20 bg-rose-500 text-white py-1 px-6 rounded border text-sm font-semibold transition-all duration-300 hover:bg-rose-600">
+                Apply
+              </button>
+            )}
+            <span className="text-gray-500">{job.company}</span>
             <p className="text-gray-500">{job.address}</p>
             <p className="font-semibold">{job.jobType}</p>
-            <p className="font-thin text-gray-500">
-              You must create an JobHunt account before continuing to the
-              company website to apply
-            </p>
             <div className="flex flex-col gap-2">
               <h2 className="text-2xl font-bold">Job Detail</h2>
               <div className="flex items-center gap-8">
-                <i className="fa-solid fa-briefcase"></i>
+                <FaBriefcase />
                 <div>
                   <h3 className="inline">Job Type</h3>
                   <p className="bg-gray-200 text-sm text-center text-slate-800 py-1 px-2 rounded">
@@ -41,13 +80,23 @@ const JobInfo = () => {
             <div className="flex flex-col gap-2">
               <h2 className="text-2xl font-bold">Location</h2>
               <div className="flex items-center gap-8">
-                <i className="fa-solid fa-location-dot"></i>
+                <FaLocationDot />
                 <p className="text-gray-500">{job.location}</p>
               </div>
             </div>
             <div className="flex flex-col gap-2">
               <h2 className="text-2xl font-bold">Full job description</h2>
               <p className="text-gray-500 text-justify">{job.description}</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-2xl font-bold">Full job responsibilities</h2>
+              <p className="text-gray-500 text-justify">
+                {job.responsibilities}
+              </p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-2xl font-bold">Full job requirements  </h2>
+              <p className="text-gray-500 text-justify">{job.requirements}</p>
             </div>
           </div>
         </section>
